2b8161c2230ecbe8c4261124217e1fcc3d4e2b5c
isaiahking14 <147957104+isaiahking14@users.noreply.github.com>
2024-01-11 16:42:22 -0800

    Add files via upload

---
+++ preLab1.java
@@ -0,0 +1,35 @@
+// CS145 Pre-Lab 1 Guessing Game
+// Isaiah King
+
+
+public class preLab1 {
+
+    // Define vars for results count
+    public static void main(String[] args) {
+        // call intro method
+        // while loop calling on game run method ex. while (gamerun = true) {
+        // }
+        // call results method
+    }
+
+    // boolean game run, calls on other methods
+        // returns game re run
+
+
+    // intro method, sysout's instructions
+
+    // number generator method
+        // Random rand = new Random();
+        // return rand.nextint(100) + 1;
+
+    // userinput method
+        // new Scanner(system.in).nextInt();
+
+    // Game re run bool method
+        // ask if player wants to play again
+        // scan, user .toLowercase(), and .startsWith().
+        
+    // overall results method
+        // sysout results
+        // call this after while method
+}   
